d8e3b7596f808f9410243c3023e20b81e3a34d9d
Amaan MK <syedkhalander66@gmail.com>
2025-04-13 13:33:30 +0530

    Update README.md

--- helpers/data_helpers.py
+++
@@ -1,6 +0,0 @@
-import csv
-
-def save_admission_request(data):
-    with open('admissions.csv', 'a', newline='') as file:
-        writer = csv.DictWriter(file, fieldnames=["student_name", "phone", "address"])
-        writer.writerow(data)
